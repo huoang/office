d519b0741d12c0732a34ec9837bca86347a36fb4
huoang <huoang@126.com>
2016-10-24 23:25:40 +0800

    1.add main golal_list. 2.getrecs modified.

---
+++ global_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#ecoding:utf-8
+
+
+strpath1501='e:/data/2015/1501.csv'
+strpath1502='e:/data/2015/1502.csv'
+strpath1503='e:/data/2015/1503.csv'
+strpath1504='e:/data/2015/1504.csv'
